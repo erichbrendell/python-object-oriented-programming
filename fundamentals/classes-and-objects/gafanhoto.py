c13e71852e76213eb55a59825b7c62b26ec7ee67
Erich Brendell <erychbrendell@gmail.com>
2026-07-15 15:18:57 -0300

    feat: improve Gafanhoto class implementation

--- fundamentals/classes-and-objects/gafanhoto.py
+++ fundamentals/classes-and-objects/gafanhoto.py
@@ -1,31 +1,48 @@
 # Declaração de Classe
 class Gafanhoto:
-    def __init__(self): # Método Construtor
+    """
+    Essa classe cria um Gafanhoto, que é uma pessoa que tem nome e idade.
+
+    Para criar uma nova pessoa, use:
+    variavel = Gafanhoto(nome, idade)
+    """
+    def __init__(self, nome = "", idade = 0): # Método Construtor
         # Atributos de Instância
-        self.nome = ""
-        self.idade = 0
+        self.nome = nome
+        self.idade = idade
 
     # Métodos de Instância
     def aniversario(self):
         self.idade = self.idade + 1
 
-    def mensagem(self):
+    # Dunder Methods
+    def __str__(self): 
         return f"{self.nome} é Gafanhoto(a) e tem {self.idade} anos de idade."
 
-# Declaração de Objetos
-g1 = Gafanhoto()
-g1.nome = "Maria"
-g1.idade = 17
-print(g1.mensagem())
-g1.aniversario()
-print(g1.mensagem())
+    def __getstate__(self):
+        return f"Estado: Nome = {self.nome} ; idade = {self.idade}"
 
-g2 = Gafanhoto()
-g2.nome = "Mauro"
-g2.idade = 53
-print(g2.mensagem())
+# Declaração de Objetos
+g1 = Gafanhoto("Maria", 17)
+print(g1)
+g1.aniversario()
+print(g1)
+
+g2 = Gafanhoto("Mauro", 53)
+print(g2)
 g2.aniversario()
-print(g2.mensagem())
+print(g2)
 
 g3 = Gafanhoto()
-print(g3.mensagem)
+print(g3)
+
+# Dunder Attributes
+print(g1.__doc__)  
+print(g1.__dict__)
+print(g2.__dict__)
+print(g1.__class__)
+print(g2.__class__)
+
+# Dunder Methods
+print(g1.__getstate__())
+print(g2.__getstate__())
